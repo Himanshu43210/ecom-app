db8f58cbd5245f5f9d954ce54dc200dee82fa9d4
Himanshu <133512069+Himanshu43210@users.noreply.github.com>
2024-01-03 20:50:15 +0530

    codechange

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,6 +6,7 @@ import { useCart } from "../context/cart";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
+import { AiOutlineReload } from "react-icons/ai";
 import "../styles/Homepage.css";
 
 const HomePage = () => {
@@ -209,7 +210,8 @@ const HomePage = () => {
                   "Loading ..."
                 ) : (
                   <>
-                    Loadmore 
+                    {" "}
+                    Loadmore <AiOutlineReload />
                   </>
                 )}
               </button>
